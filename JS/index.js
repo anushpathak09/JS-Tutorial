adff0e8ef698b9b78f5567bdcd98ab12900b5dc8
Anush Pathak <119616148+anushpathak09@users.noreply.github.com>
2023-04-17 10:55:03 +0545

    JS Tutorial DAY-1 JS code

---
+++ JS/index.js
@@ -0,0 +1,17 @@
+// 1. Ways to print in JS 
+    console.log("hello world");
+        // alert("me"); --->NOT USED IN MODERN JAVA SCRIPT MUCH
+            document.write("this is document write");
+
+// 2. javascript console API (Application  Programming interface)
+    // add text to console 
+        console.warn("this is warning");
+        console.error("this is an error");
+        console.clear("to clear all consoles text");
+
+        // console.assert("to check value is equal or not")
+            // for example 
+                console.assert(4==4) 
+                console.assert(4==(6-2))
+
+
