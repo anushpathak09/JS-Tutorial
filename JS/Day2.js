8456bbf9bb5cbe93a83e866a3d32dab5d5cce418
Anush Pathak <119616148+anushpathak09@users.noreply.github.com>
2023-04-19 13:44:53 +0545

    DAY-2 JS Learning tutorial
    Today, I learnt about data types in JavaScript.

---
+++ JS/Day2.js
@@ -0,0 +1,59 @@
+                    // DAY-2 
+
+// 3. javascript Variables
+    // containers to store datas value is variables.
+
+    var number1 = 34;
+    var number2 = 56;
+
+    console.log( number1 + number2);
+
+// 4. Data Types in JS 
+
+// ---> Primitive data types.
+
+// Numbers 
+
+var num1=55;
+var num2=66;
+
+// String
+
+    // we write string in double or single cot 
+        var str1 = "str stands for strings which is used for take character as input. ";
+        var str2 = 'str stands for strings which is used for take character as input. ';
+
+// Object
+var marks = {
+    rahul: 66,
+    abhishek: 54,
+    anush: 99.99
+}
+
+// printing codes in console log 
+console.log(num1);
+console.log(num2);
+console.log(marks);
+console.log(number1);
+console.log(number2);
+
+// Booleans
+var a = true;
+var b = false;
+console.log(a,b);
+
+/* 
+
+There are two types of data types in javascript they are:-
+1.Primitive data type.
+---> undefined, null, numbers, string, boolean, symbols.
+2.Reference data type.
+---> Arrays & Objects.
+
+*/
+
+// Reference data types. 
+    
+    // Arrays
+        var array = [1,2,3,4,5,6]
+        console.log(array [3])
